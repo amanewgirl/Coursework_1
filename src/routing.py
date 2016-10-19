e0e7119aaadecbc0beda082541f6a748f29a4d9f
amanewgirl <amantlecraviolatti@gmail.com>
2016-10-19 15:30:39 +0000

    full tree structure of web app and templates

---
+++ src/routing.py
@@ -0,0 +1,21 @@
+# Coursework:Google doodle site structure
+
+from flask import Flask, redirect, url_for, abort,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+  return render_template ('home.html')
+
+@app.route("/Search-a-doodle/")
+def hello():
+  return "If you want to search for doodles, you've found the right place"
+
+@app.route("/more-doodles")
+def goodbye():
+  return "View all doodles"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+
