7c113a10adb950e5c28998778e522f90113eddff
amanewgirl <amantlecraviolatti@gmail.com>
2016-11-04 14:16:25 +0000

    Commenting

--- src/routing.py
+++ src/routing.py
@@ -1,8 +1,7 @@
 # Coursework:Google doodle site structure
 
 import os
-from flask import Flask, redirect, url_for, abort,render_template, send_from_directory
-from glob import iglob
+from flask import Flask, redirect, url_for, abort,render_template, send_from_directory, request
 from os.path import basename
 app = Flask(__name__)
 
@@ -10,35 +9,70 @@ app = Flask(__name__)
 def design():
   return render_template ('standard.html')
 
-#This gets all the still doodles to be used across the web app
-def jpgs():
-   images = os.listdir('./static/all_doodles/still_doodles')
-   print(images)
-   return render_template ('imageload.html', images=images)
 
 #this is the homepage of the app
 @app.route("/")
 def root():
-  images = os.listdir('./static/all_doodles/still_doodles')
-  print(images)
-  return render_template ('newhome.html',images=images)
+    images = os.listdir('./static/all_doodles/still_doodles')
+    return render_template ('home.html',images=images)
 
 
-#this lists all available doodles
+#this lists all available doodles and returns more categories if signed in
 @app.route("/all-doodles/")
-def alldoodles():
-   images = os.listdir('./static/all_doodles/still_doodles')
-   print(images)
-   return render_template ('imageload.html', images=images)
+@app.route("/all-doodles/<name>")
+def alldoodles(name=None):
 
+#Opens the directory for jpgs
+    pathstill = "./static/all_doodles/still_doodles"
+    images = os.listdir(pathstill)
+
+    for file in images:
+      print file
+
+#Opens the directory for gifs
+    pathgifs = "./static/all_doodles/gifs"
+    gifs = os.listdir(pathgifs)
+
+    for file in gifs:
+      print file
+
+    return render_template ('imageload.html', images=images, name=name, gifs=gifs)
+
+
+
+#The latest doodle from Google is shown here
 @app.route("/latest-doodle/")
 def newdoodle():
-   return render_template ('home.html')
+   return render_template ('latest.html')
 
 
-@app.route("/test")
-def testing():
-   return render_template ('test.html')
+#Allows temporary sign in to access hidden categories
+@app.route('/account')
+def account():
+   return render_template('account.html')
+
+@app.route("/signedin", methods = ['POST', 'GET'])
+def signedin():
+   if request.method == 'POST':
+      signedin = request.form 
+      return render_template("sign.html",signedin = signedin)
+
+
+#Allows single picture uploads for signed in  user
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+
+@app . route ("/uploading", methods =[ 'POST','GET'])
+def uploading():
+  if request . method == 'POST':
+  	f = request.files ['file']
+	f.save ('static/uploads/upload.png')
+        return " File Uploaded "
+ 
+
+
+
 
 
 
